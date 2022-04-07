e039dda876267270ca6cf41f40cb25298ca4a9b8
lizilong <2791778524@qq.com>
2022-04-07 11:15:12 +0800

    提交代码

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,15 +1,51 @@
 <template>
-  <view>
-      11111
+  <view id="container">
+      <view id="content">
+        <header>
+          <p class="goback" @click="goBaidu">&lt;</p>
+          <p class="center">Hello UniApp</p>
+          <p class="main">我的</p>
+        </header>
+        <main>
+
+        </main>
+      </view>
   </view>
 </template>
 
 <script>
 export default {
-
+  methods:{
+    goBaidu() {
+      location.href = 'http://baidu.com'
+    }
+  }
 }
 </script>
 
 <style>
-
+#content header{
+  font-size: 3.5vw;
+  text-align: center;
+  background: #bbb;
+  color: #000;
+  height: 5vh;
+  line-height: 5vh;
+  display: flex;
+  justify-content: space-between;
+}
+#content header .gobaack{
+  width: 2vw;
+  text-align: center;
+  font-weight: bold;
+}
+#content header .center{
+  position: absolute;
+  width: 100%;
+  text-align: center;
+}
+#content header .main{
+  width: 10vw;
+  text-align: center;
+}
 </style>
